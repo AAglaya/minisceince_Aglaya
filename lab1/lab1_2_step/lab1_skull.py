e8d3794ade7056247cd504187fe4384465d20fa6
AAglaya <59313041+AAglaya@users.noreply.github.com>
2024-03-02 13:18:37 +0300

    Добавлен 2 этап

---
+++ lab1/lab1_2_step/lab1_skull.py
@@ -0,0 +1,26 @@
+import gmsh
+import sys
+import os
+import math
+
+gmsh.initialize()
+
+gmsh.model.add("skull")
+
+path = os.path.dirname(os.path.abspath(__file__))
+gmsh.merge(os.path.join(path, "Skull.stl"))
+
+gmsh.model.mesh.classifySurfaces(180 * math.pi/180, True, True, 180 * math.pi /180)
+
+s = gmsh.model.getEntities(2)
+l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
+gmsh.model.geo.addVolume([l])
+
+gmsh.model.geo.synchronize()
+
+gmsh.model.mesh.generate(3)
+
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
+
+gmsh.finalize()
